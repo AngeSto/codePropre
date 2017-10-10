88a5ef85fd73c1840e8166458ec64161a08a5775
Ange STOIANOV <stoianov.ange@hotmail.fr>
2017-10-10 18:07:54 +0200

    Ex 6 Done

--- src/main/java/ex6/Conteneur.java
+++ src/main/java/ex6/Conteneur.java
@@ -8,19 +8,27 @@ public class Conteneur {
 	private int size;
 	
 	public void add(Object element){
-		if (!readOnly){
+		if (readOnly){
+		return;
+		}
 			int newSize = size+1;
 			if (newSize > size){
-				Object[] newElements = new Object[size+10];
-				for (int i=0; i<size; i++){
-					newElements[i] = elements[i];
-				}
-				elements = newElements;
+				aggrandirTableau();
 			}
 			elements[size++]=element;
 		}
-		
+
+
+
+	private void aggrandirTableau() {
+		Object[] newElements = new Object[size+10];
+		for (int i=0; i<size; i++){
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
 	}
+		
+	
 	
 	public Object getContent(int i){
 		return elements[i];
